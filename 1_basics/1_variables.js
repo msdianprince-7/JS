d337188f80106d250c26486066611f94df26c883
msdianprince-7 <136072927+msdianprince-7@users.noreply.github.com>
2024-02-16 05:52:06 +0000

    variables

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144533
+let accountEmail = "nandwana@.com"
+
+var accountPassword = "12345"
+
+let accountState;  // aise choddo to undefined value le leta hai  
+
+
+accountCity = "jaipur"
+
+accountEmail= "anannna"
+
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+
+accountPassword = "23456"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" ; // treat all js code as new version
+
+// alert (3+3) // we are using node js not browser 
+
+// code readibility should be high 
+
+
+// dattaypes //
+// number => 2 to power 53 
+// bigint => 
+
+// string => " "
+//boolean => true/false
+// null => standalone value
+// undefined  => vale assingn nhi hai 
+// symbol  => unique 
+
+// object 
+
+console.log(typeof null);
+console.log(typeof undefined);
+
