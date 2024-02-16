e8496eb96546c7f46a6b1116462b5c513af0b331
msdianprince-7 <136072927+msdianprince-7@users.noreply.github.com>
2024-02-16 07:51:32 +0000

    comparison

---
+++ 1_basics/4_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);    ye sab basic hai 
+
+
+ console.log("2">1);    // datatype change fasne k bhi chances hai 
+ console.log("02">1);
+ 
+console.log(null>=0);
+console.log(null==0);
+console.log(null<0);
+
+console.log(undefined>=0);
+console.log(undefined==0);
+console.log(undefined<0);   // iss tupe k conversion nhi karne chahiye 
+
+
+// == only checks value 
+// === checks datatype +  value 
