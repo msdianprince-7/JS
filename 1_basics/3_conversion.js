7b5127717c407c6c03efd40ace7134ca3146db13
msdianprince-7 <136072927+msdianprince-7@users.noreply.github.com>
2024-02-16 06:17:39 +0000

    conversion

---
+++ 1_basics/3_conversion.js
@@ -0,0 +1,31 @@
+let score ="33abc";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN(type iska number hi hai )
+// true = 1
+// false =0
+
+
+let loggedIn = 1;
+
+let booleanLoggedIn= Boolean(loggedIn);
+console.log(booleanLoggedIn);
+
+// 1 =>true 
+// "" => false 
+// "prince" => true 
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
