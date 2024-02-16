7001b45becd8f319a2e2a9991f9bec9ba212af0d
msdianprince-7 <136072927+msdianprince-7@users.noreply.github.com>
2024-02-16 10:36:55 +0000

    math and num

---
+++ 1_basics/6_nums_maths.js
@@ -0,0 +1,65 @@
+const score = 100;
+// console.log(score);
+const age = new Number(24);
+// console.log(age);
+
+// console.log(age.toString()); // ab ye string ban gaya hai
+// console.log(age.toFixed(4));
+
+const  otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// *********************** Maths****************
+
+// console.log(Math);
+// console.log(Math.abs(-45));
+// console.log(Math.round(6.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.floor(4.8));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(2,5,3,4,6));
+// console.log(Math.max(2,5,3,4,6));
+
+
+console.log(Math.random())  // 0 se 1 k beech mein value dega 1 nahi dega 
+ 
+console.log(Math.floor(Math.random()*10)+1);
+
+const max =10 ;
+const min =20;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
