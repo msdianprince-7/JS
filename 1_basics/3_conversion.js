5dc937c9cce520cd69a2f16cc4775c8db92891db
msdianprince-7 <136072927+msdianprince-7@users.noreply.github.com>
2024-02-16 06:44:00 +0000

    conversion

--- 1_basics/3_conversion.js
+++ 1_basics/3_conversion.js
@@ -1,4 +1,4 @@
-let score ="33abc";
+/*let score ="33abc";
 
 console.log(typeof score);
 console.log(typeof(score));
@@ -21,11 +21,59 @@ console.log(booleanLoggedIn);
 // 1 =>true 
 // "" => false 
 // "prince" => true 
+*/
 
 
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+//  ******** Operations ************
+
+let value =3;
+let negValue = -value; 
+//console.log(negValue);
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)  2 raise to the power 2
+
+
+let str1 = "hello";
+let str2= " priyansh";
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+// pehle string hai to pehle string mein merge kiya jayega 
+// aur pehle number hai to phir sabko number treat kiya jayega 
+
+
+// console.log((3+4)*(5%3));
+
+console.log(+true);
+console.log(+"");
+
+
+let num1 ,num2,num3;
+
+num1 = num2 =num3 = 4;
+
+let gameCounter = 100;
+gameCounter++;       // postfix or prefix 
+console.log(gameCounter);
+
+
 
